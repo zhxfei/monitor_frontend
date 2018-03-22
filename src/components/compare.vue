dcfc2262fe47bbdd6b43aabe792aea021fbc2520
zhxfei <dylan@zhxfei.com>
2018-03-23 00:20:27 +0800

    add compare

---
+++ src/components/compare.vue
@@ -0,0 +1,51 @@
+<template>
+      <el-main>
+
+        <searchform/>
+        <!-- 展示组件 -->
+
+            <el-row>
+                <el-col :span="24">
+                    <multihost echartsid="mutihostcharts"></multihost>
+                </el-col>
+            </el-row>
+
+      </el-main>
+</template>
+
+<style>
+  .el-header {
+    /* background-color: rgb(0, 128, 100); */
+    color: #333;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    color: #333;
+  }
+
+  .el-main {
+    width: 100%
+  }
+  .span{
+    font-family: "Helvetica Neue",
+  }
+</style>
+
+<script>
+import searchform from './searchform'
+import multihost from './multihost'
+// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+  export default {
+    name: 'compare',
+    data() {
+      return {
+      }
+    },
+    components: {
+      searchform,
+      multihost
+  },
+};
+</script>
---
+++ src/components/multihost.vue
@@ -0,0 +1,202 @@
+<template>
+  
+  <div class="chart-container">
+      <div :id="this.echartsid" style="height: 400px" >图表加载失败</div>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+import axios from 'axios'
+import qs from 'qs'
+
+export default {
+    name: 'multiline',
+    data(){
+        return  {
+            myChart : '',
+            showHost: ['newbie-unknown85822.i.nease.net', 'newbie-unknown85823.i.nease.net'],   // default
+            showItem: "cpu.idle.percent",
+            // id_name : 'gauge1',
+            showdate: [],
+            showdata: [],
+            timeRange: []
+    
+        }
+    },
+    props: [
+        'echartsid'  
+    ],
+    watch:{
+        showdata: {
+            handler(){
+                this.chart.setOption(this.getOption());
+                this.chart.hideLoading();
+            }
+        },
+    },
+    methods: {
+        getOption(){
+            let option = {
+                    title: {
+                        text: this.getTitle()
+                    },
+                    tooltip: {
+                        trigger: 'axis'
+                    },
+                    legend: {
+                        data: this.showHost
+                    },
+                    grid: {
+                        left: '3%',
+                        right: '4%',
+                        bottom: '3%',
+                        containLabel: true
+                    },
+                    toolbox: {
+                      feature: {
+                          restore: {},
+                          saveAsImage: {}
+                      }
+                    },
+                    xAxis: {
+                        type: 'category',
+                        boundaryGap: false,
+                        data: this.showdate
+                    },
+                    yAxis: this.getYaxis(),
+                    series: this.showdata
+            };
+            return option
+        },
+        getTitle(){
+            return this.showItem + "同类指标对比"
+        },
+        getYaxis(){
+          let args = this.showItem.split('.')
+          if (args[args.length-1] === 'percent' || args[3] === 'percent'){
+            return {
+              type : 'value',
+              axisLabel : {
+                formatter: '{value} %'
+              }
+            }
+          }
+          else if (args[0] == 'mem'){
+            return {
+              type : 'value',
+              axisLabel : {
+                formatter: '{value} M'
+              }
+            }
+          }else{
+            return {
+              type : 'value',
+              axisLabel : {
+                formatter: '{value}'
+              }
+            }
+          }
+      },
+        dateFormat (t1) {
+            var newDate = new Date();
+            newDate.setTime(t1 * 1000);
+            return [
+              newDate.getFullYear(), 
+              newDate.getMonth()+1,     // 这是个坑....
+              newDate.getDate()
+              ].join("/")+' '+ [
+              newDate.getHours(), 
+              newDate.getMinutes()].join(':');
+        },
+        getUserChartInit() {
+            this.getDataInit()
+
+            this.chart = echarts.init(document.getElementById(this.echartsid));
+            this.chart.showLoading()
+            this.chart.setOption(this.getOption());
+            this.chart.hideLoading();
+        },
+        getDataInit(){
+            this.showdata = []
+            this.showdate = []
+
+        },
+        genShowData(metric_name, data){
+            let rData = [];
+            for (var obj of data) {
+                rData.push(this.getDataFormater(metric_name, obj.value));
+            }
+            if (this.showdate.length == 0){
+                for (var obj of data) {
+                    this.showdate.push(this.dateFormat(obj.timestamp));
+                }    
+            }
+            return rData;
+        },
+        getDataFormater(metric_name, data){
+          let args = metric_name.split('.')
+          let metricType = args[0]
+          let percent = args[args.length-1]
+          if (metricType == 'mem' && percent !== 'percent'){
+            return Math.floor(data/1024/1024)
+          }else{
+            return data
+          }
+        },
+        // eventHandler
+        compareEventHandler(formData){
+            this.showdate = []
+            this.showdata = []
+            console.log('compare')
+            this.showHost = formData.chosedhost
+            this.showItem = formData.chosedItem
+            this.timeRange = formData.timeRange
+            for (let hostName of this.showHost){
+                let req_data = {
+                    endpoint: hostName,
+                    metric: this.showItem,
+                    s_time: Date.parse(this.timeRange[0])/ 1000,
+                    e_time: Date.parse(this.timeRange[1])/ 1000
+                }
+                let req_info = qs.stringify(req_data);
+
+                axios.post('http://10.83.3.46:11111/monitor/v1/items', req_info,
+                    {
+                        headers:{
+                            'Content-type': 'application/x-www-form-urlencoded',
+                            'Accept': '*/*'
+                        }
+                    })
+                .then((res) => {
+                    this.showdata.push({
+                        name: hostName,
+                        type:'line',
+                        data: this.genShowData(this.showItem, res.data)
+                    });
+                })
+                .catch((err)=>{
+                    console.log(err)
+                })
+            }
+        },
+    },
+    mounted () {
+      this.$nextTick(function () {
+        this.getUserChartInit();
+      })
+    },
+    created() {
+        this.$bus.$on('compare-event', this.compareEventHandler)
+    },
+    beforeDestroy() {
+      this.$bus.$off('compare-event', this.handleMyEvent)
+    }
+
+}
+
+</script>
+
+<style>
+
+</style>
